eeb360c4fec03bfbed0c95043b7e0a455c919b43
Rayan Lbd <rayanlbd@MacBook-Air-de-Rayan.local>
2024-02-08 15:07:16 +0100

    merge des use states

--- fare-rule-normalization-and-automation-main/final_version/client/components/PnrUpload.tsx
+++ fare-rule-normalization-and-automation-main/final_version/client/components/PnrUpload.tsx
@@ -1,31 +1,15 @@
-import React, { useState, ChangeEvent, useRef } from "react";
+import React, { useState, ChangeEvent, useRef, useEffect } from "react";
 import { FaRegFilePdf } from "react-icons/fa6";
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
 import { useChatStore } from "./chatStore";
 import { useLoadingStore } from "./Loading";
-import { Input } from "@/components/ui/input";
 import {
-  Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area-shadcn";
-import {
-  Form,
-  FormControl,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { Calendar } from "@/components/ui/calendar";
-import { CalendarIcon } from "lucide-react";
+import { ScrollArea } from "@/components/ui/scroll-area-shadcn";
 
 const truncateFileName = (fileName: string, maxLength: number) => {
   return fileName.length > maxLength
@@ -60,33 +44,33 @@ type AnswerType = {
   updates: UpdateType[];
 };
 
-export function PnrUpload() {
+export function PnrUpload({
+  setPnrInfo,
+  pnrInfo,
+}: {
+  setPnrInfo: React.Dispatch<React.SetStateAction<string | null>>;
+  pnrInfo: string | null;
+}) {
   const { addToHistory } = useChatStore();
   const { setIsLoading } = useLoadingStore();
   const [pnrSummary, setPnrSummary] = useState<string | null>(null);
-  let pnrData: AnswerType | null = null;
-  let pnrSum: PassengerData | null = null;
-  if (pnrSummary !== null) {
-    pnrData = JSON.parse(pnrSummary);
-    if (pnrData !== null) {
-      pnrSum = pnrData["summary"];
-    }
-  }
   const [selectedPnr, setSelectedPnr] = useState<File | null>(null);
   const pnrInputRef = useRef<HTMLInputElement>(null);
+  let pnrData: AnswerType | null = null;
+  let pnrSum: PassengerData | null = null;
+
   console.log("pnrInputRef : ", pnrInputRef);
+
+  //Fonction pour envoyer le PNR au back et récupérer le summary et la timeline
   const uploadPnr = async (pnr: File) => {
+    let response;
     setIsLoading(true);
     const formData = new FormData();
     formData.append("pnr", pnr);
-
     const urls = [
       "http://127.0.0.1:8000/upload_pnr",
       "https://f23-p3-amadeus.paris-digital-lab.fr/back/upload_pnr",
     ];
-
-    let response;
-
     for (const url of urls) {
       try {
         response = await fetch(url, {
@@ -98,12 +82,8 @@ export function PnrUpload() {
           try {
             const data = await response.json();
             console.log("data2 : ", data.paragraph);
-            setPnrSummary(data.paragraph);
-            // addToHistory({
-            //   content: data.paragraph,
-            //   role: "assistant",
-            //   id: "pdf",
-            // });
+            setPnrInfo(data.paragraph);
+            // setPnrSummary(data.paragraph);
             setIsLoading(false);
           } catch (error) {
             console.error("Error uploading pnr:", error);
@@ -117,15 +97,26 @@ export function PnrUpload() {
     }
   };
 
+  //Code qui définit le summary et la timeline
+  if (pnrInfo !== null) {
+    pnrData = JSON.parse(pnrInfo);
+    if (pnrData !== null) {
+      pnrSum = pnrData["summary"];
+      console.log("pnrSum : ", pnrSum);
+      console.log("timeline : ", pnrData["updates"]);
+    }
+  }
+
+  //Fonction pour uploader un PNR
   const handlePnrChange = (event: ChangeEvent<HTMLInputElement>) => {
     const pnr = event.target.files?.[0];
     setSelectedPnr(pnr || null);
-
     if (pnr) {
       uploadPnr(pnr);
     }
   };
 
+  //Fonction pour supprimer un PNR
   const handleDeletePnr = () => {
     setSelectedPnr(null);
     // Reset the pnr input value
@@ -183,7 +174,7 @@ export function PnrUpload() {
           </div>
         )}
       </div>
-      {pnrSummary && selectedPnr && (
+      {pnrInfo && selectedPnr && (
         <ScrollArea type="always" style={{ height: 500 }}>
           <p className="text-secondary font-semibold">Passengers :</p>
           <ul>
